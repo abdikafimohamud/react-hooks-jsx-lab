3830b63068909c28e2829b2706b9d22035e280f4
abdikafimohamud <185328738+abdikafimohamud@users.noreply.github.com>
2025-04-21 07:07:34 -0700

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
+import React from 'react';
+import { image } from '../data';  // Corrected import path
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about myself.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
